162006c22142dc6ef2dece44bc3e8d3583e59949
GabrielWelvaert <gabewelvaert@gmail.com>
2025-07-12 13:59:27 -0400

    react tailwind

--- src/App.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function App() {
-  return (
-    <div className="text-3xl text-red-600 text-center mt-10">
-      Test
-    </div>
-  );
-}
---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import { LoadingScreen } from './components/LoadingScreen'
+import "./index.css"
+
+function App() {
+  // const [isLoaded, setIsLoaded] = useState(false)
+  return (
+    <>
+      <LoadingScreen/>
+    </>
+  )
+}
+
+export default App
