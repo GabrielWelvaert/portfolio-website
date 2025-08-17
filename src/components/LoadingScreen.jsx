adc0750b36c7f5a6dc30f559eae6f3a74cd3b6be
GabrielWelvaert <gabewelvaert@gmail.com>
2025-08-17 11:04:44 -0400

    testing

--- src/components/LoadingScreen.jsx
+++ src/components/LoadingScreen.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 
 export const LoadingScreen = () => {
     const [text, setText] = useState("");
-    const fullText = "<Hello World/>";
+    const fullText = "Welcome To My Website!!!";
     useEffect(() => {
         let index = 0;
         const interval = setInterval(() => {
@@ -10,16 +10,17 @@ export const LoadingScreen = () => {
             index++;
             if(index > fullText.length){
                 clearInterval(interval)
+                document.getElementById("kill").innerText = "";
             }
-        }, 100);
+        }, 50);
         return () => clearInterval(interval);
-    })
+    }, [])
 
     return (
         // outmost div is a flexbox (column) of fixed centered elements
         <div className="fixed inset-0 z-50 bg-black text-gray-100 flex flex-col items-center justify-center">
-            <div className="mb-4 text-4xl font-mono font-bold">
-                {text} <span className="animate-blink ml-1"> | </span>
+            <div className="mb-4 text-6xl font-mono font-bold">
+                {text}<span id="kill" className="animate-blink text-6xl ml-1">|</span>
             </div>
             <div className="w-[200px] h-[2px] bg-gray-800 rounded relative overflow-hidden">
                 <div className="w-[40%] h-full bg-blue-500 shadow-[0_0_15px_#3b82f6] animate-loading-bar"></div>
