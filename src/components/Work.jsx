1c24e7bec3e2b2a8cabe676ea5cf02c2344515b8
GabrielWelvaert <gabewelvaert@gmail.com>
2025-09-21 11:17:36 -0400

    language

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -12,7 +12,7 @@ export const Work = () => {
                     Although I am technically a junior, I've had the great opportunity to serve as the primary developer for this LAMP stack application (RHEL9; AWS hosted), handling the bulk of the development work through multiple major releases and hotfixes. I've played a key role in guiding the team through penetration testing and audits, ensuring our systems meet security & accessibility compliance standards and that we demonstrate competency for contract recompete purposes. I regularly communicate with leaders, business analysts, and testers, explaining the code and outlining what needs to be done to maintain and improve the system.
                 </div>
                 <div className="passage-text">
-                    I leverage tools such as Git, GitHub, Fortify, AWS, Docker, and VSCode to efficiently deliver high-quality & maintainable solutions. My current aspirations are to migrate legacy PHP scripts to Node.js microservices to modernize the system and ensure it remains future-proof, and to move our CI/CD pipeline and monitoring processes fully to AWS.
+                    I leverage tools such as Git, GitHub, Fortify, AWS, Docker, and VSCode to efficiently deliver high-quality & maintainable solutions. My current priorities are migrating legacy PHP scripts to Node.js microservices to modernize the system and ensure it remains future-proof, and to move our CI/CD pipeline and monitoring processes fully to AWS.
                 </div>
             </div>
         </>
