3f184b4a8634b25b6d1ff524bcfb193a21426b85
GabrielWelvaert <gabewelvaert@gmail.com>
2025-10-12 19:19:03 -0400

    language

--- src/components/Gabebook.jsx
+++ src/components/Gabebook.jsx
@@ -33,12 +33,15 @@ export const Gabebook = ({ className }) => {
                 <div className="passage-text">Once the response is received, the view will automatically update:</div>
                 <img className="rounded-image" src="/likepostuireturned.png"></img>    
             </div>
-            <div className="passage-text">
-                Controllers can access any models they need. For example, the likePost controller as seen above uses queries from the PostModel, LikesModel, and UserModel.
-                This allows models to be specific and used by a variety of controllers, which helps with code maintainability and expandability.
+            <div className="flex flex-col justify-center items-center">
+                <div className="passage-text">
+                    Controllers can access any models they need. For example, the likePost controller as seen above uses methods from the PostModel, LikesModel, and UserModel.
+                    This allows model methods to stay specific while being used across multiple controllers, improving code maintainability and extensibility. For example, here are a few methods from the LikesModel:
+                </div>
+                <img className="rounded-image" src="/likesmodel.png"></img>  
             </div>
             <div className="passage-text">
-                This request/response pattern, which dynamically updates views, drives most of the application's core functionality that users directly interact with. Now, we'll explore some interal server-side features:
+                This request/response pattern, which dynamically updates views, drives most of the application's core functionality that users directly interact with (posting, commenting, messaging, etc.). Now, we'll explore some interal server-side features:
             </div>
             <div className="passage-text">
                 authentication, authorization, custom middleware
